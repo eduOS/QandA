538eb11342bf08043ba9b28268f1598d2cb78ac7
eduOS <51130104064@ecnu.cn>
2015-07-14 17:45:06 +0800

    need to be further modified

--- QandA.py
+++ QandA.py
@@ -8,95 +8,122 @@ from bs4 import BeautifulSoup
 import re,sys
 import requests
 import time
+from datetime import datetime
 import MySQLdb as mdb
 
 con = mdb.connect('localhost',"root","104064",'QandA')
 cur = con.cursor()
 
+fakeID = 1000000
 time.strftime("%Y-%m-%d")
 # update automatically a week later / every Tuesday
 
-pro_soup = BeautifulSoup(requests.get('http://www.abc.net.au/tv/qanda/past-programs-by-date.htm').text)
+def create_tables():
+    cur.execute('DROP TABLE IF EXISTS hentry')
+    cur.execute(
+        "CREATE TABLE `hentry` ("
+        "   `epiShortNumber` VARCHAR(10) NOT NULL,"
+        "   `hentryDate` VARCHAR(30) NOT NULL,"
+        "   `entryTitle` VARCHAR(60) NOT NULL,"
+        "   `bookmark` VARCHAR(300) NOT NULL,"
+        "   `videoLink` varchar(100),"
+        "   `questions` TEXT NOT NULL,"
+        "   `transcript` MEDIUMTEXT NOT NULL,"
+        "   PRIMARY KEY (`shortNumber`)"
+        ") engine=InnoDB")
+    
+    cur.execute('DROP TABLE IF EXISTS henPan')
+    cur.execute("""CREATE TABLE henPan(epiShortNumber VARCHAR(10) NOT NULL, \
+                                     panelNameTag VARCHAR(50) NOT NULL)""")
+    
+    cur.execute('DROP TABLE IF EXISTS panellist')
+    cur.execute('CREATE TABLE panellist(panelNameTag VARCHAR(50) NOT NULL PRIMARY KEY, panelName VARCHAR(40) NOT NULL, panelPhotoNumber VARCHAR(10), panelProfile VARCHAR(5000) NOT NULL)')
+    #                                    panellIdentity VARCHAR(40), \
 
-hentries = pro_soup.find_all('div', class_ = 'hentry')
+def retrieve_entries():
+    pro_soup = BeautifulSoup(requests.get('http://www.abc.net.au/tv/qanda/past-programs-by-date.htm').text)
+    entries = pro_soup.find_all('div', class_ = 'hentry')
+    # pass one date and get all new entries only
+    return entries
 
-cur.execute('DROP TABLE IF EXISTS hentry')
-cur.execute(
-    "CREATE TABLE `hentry` ("
-    "   `hentryDate` VARCHAR(30) NOT NULL,"
-    "   `entryTitle` VARCHAR(60) NOT NULL,"
-    "   `bookmark` VARCHAR(300) NOT NULL,"
-    "   `videoLink` varchar(100),"
-    "   `questions` TEXT NOT NULL,"
-    "   `transcript` MEDIUMTEXT NOT NULL,"
-    "   PRIMARY KEY (`hentryDate`)"
-    ") engine=InnoDB")
+def dump_the_hentry(hentry):
+    'insert all into hentry table'
+    date = hentry.find('span', class_ = 'date').string.encode('UTF-8')
+    date = datetime.strptime(date,'%A %d $B, %Y')
+    epi_link = hentry.find('a', class_ = 'details')['href'].encode('UTF-8')
+    epiShortNumber = epi_link[-11:-4]
+    bookmark = hentry.find('a', class_ = 'entry-title').string.encode('UTF-8')
+    epi_soup = BeautifulSoup(requests.get(epi_link).text)
+    videoLink = epi_soup.find('li', class_ = 'download')
 
-cur.execute('DROP TABLE IF EXISTS henPan')
-cur.execute("""CREATE TABLE henPan(hentryDate VARCHAR(30) NOT NULL, \
-                                 panellID VARCHAR(10) NOT NULL)""")
+    if videoLink:
+        videoLink = videoLink.find('a')['href'].encode('UTF-8')
+    else:
+        fakeID += 100
+        videoLink = str(fakeID)
 
-cur.execute('DROP TABLE IF EXISTS panellist')
-cur.execute('CREATE TABLE panellist(panellID VARCHAR(10) NOT NULL PRIMARY KEY, panelName VARCHAR(40) NOT NULL, panellProfile VARCHAR(5000) NOT NULL)')
-#                                    panellIdentity VARCHAR(40), \
+    questions = epi_soup.find('div', id = 'questions').text.encode('UTF-8')
+    transcript = epi_soup.find('div', id = 'transcript').text.encode('UTF-8')
 
-fakeID = 1000000
+    sql = 'INSERT INTO hentry VALUES(%s,%s,%s,%s,%s,%s,%s)'
+    cur.execute(sql,(epiShortNumber,date,epi_link,bookmark,videoLink,questions,transcript,))
 
-try:
-    for hentry in hentries:
-        date = hentry.find('span', class_ = 'date').string.encode('UTF-8')
-        print date
-        epi_link = hentry.find('a', class_ = 'details')['href'].encode('UTF-8')
-        bookmark = hentry.find('a', class_ = 'entry-title').string.encode('UTF-8')
-        #panelIden = find('div', class_ = 'entry-summary').string.encode('UTF-8')
+    return epi_soup
 
-        epi_soup = BeautifulSoup(requests.get(epi_link).text)
+def dump_panellist(epi_soup):
 
-        videoLink = epi_soup.find('li', class_ = 'download')
-        if videoLink:
-            videoLink = videoLink.find('a')['href'].encode('UTF-8')
+    presenters = epi_soup.find_all('div', class_ = 'presenter')
+
+    for presenter in presenters:
+        panel_panel_ID = presenter.find('img')
+        if panel_ID:
+            panel_ID = ID['src'][-11:-4]
         else:
-            fakeID += 100
-            videoLink = str(fakeID)
-            print fakeID
+            panel_ID = str(fakeID)
+            fakepanel_ID += 1
+        name = presenter.find('a').text.encode('UTF-8')
+        profile = presenter.find('p').text.encode('UTF-8')
 
-        questions = epi_soup.find('div', id = 'questions').text.encode('UTF-8')
-        transcript = epi_soup.find('div', id = 'transcript').text.encode('UTF-8')
-        sql = 'INSERT INTO hentry VALUES(%s,%s,%s,%s,%s,%s)'
-        cur.execute(sql,(date,epi_link,bookmark,videoLink,questions,transcript,))
+        sql = 'INSERT INTO henPan VALUES(%s,%s)'
+        cur.execute(sql,(epi_ID,panel_ID,))
+        # the table henpan shoulbe be modified: making episode number and panellist's panel_ID as foreign key
 
-        presenters = epi_soup.find_all('div', class_ = 'presenter')
-        for presenter in presenters:
+        sql = 'SELECT * FROM panellist WHERE panellID = %s'
+        cur.execute(sql,(panel_ID,))
+        if not cur.rowcount:
+            sql = 'INSERT INTO panellist VALUES(%s,%s,%s)'
+            cur.execute(sql,(panel_ID,name,profile,))
 
-            ID = presenter.find('img')
-            if ID:
-                ID = ID['src'][-11:-4]
-            else:
-                ID = str(fakeID)
-                fakeID += 1
-                print ID
 
-            name = presenter.find('a').text.encode('UTF-8')
+class QandA:
+    @staticmethod
+    def init_database():
+        # create table
+        create_tables()
+        # retrieve all entries from the home page
+        hentries = retrieve_entries()
+        
+        # insert all data
+        
+        try:
+            # fetch details of each episode via the link from each entry
+            for hentry in hentries:
+                #panelIden = find('div', class_ = 'entry-summary').string.encode('UTF-8')
+                entry_info = fetch_info_from_entry(hentry)
 
-            profile = presenter.find('p').text.encode('UTF-8')
+                epi_info = fetch_from_epi(entry_info['epi_link'])
 
-            sql = 'INSERT INTO henPan VALUES(%s,%s)'
-            cur.execute(sql,(date,ID,))
-
-            sql = 'SELECT * FROM panellist WHERE PanellID = %s'
-            cur.execute(sql,(ID,))
-            if not cur.rowcount:
-                sql = 'INSERT INTO panellist VALUES(%s,%s,%s)'
-                cur.execute(sql,(ID,name,profile,))
-except:
-    con.commit()
-    cur.close()
-    con.close()
-    print 'error occurs', date
-    sys.exit(0)
-
-con.commit()
-cur.close()
-con.close()
-
+        
+        
+        except:
+            con.commit()
+            cur.close()
+            con.close()
+            print 'error occurs', date
+            sys.exit(0)
+        
+        con.commit()
+        cur.close()
+        con.close()
+        
 # cannot fetch data in 2008
